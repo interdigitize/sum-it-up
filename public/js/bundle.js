0f58295afde0e1e2985fe1236b15eb4ce27c3f50
interdigitize <interdigitize@users.noreply.github.com>
2017-04-29 23:27:10 -0700

    adds rendering data rows to the DOM

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -74,7 +74,7 @@ module.exports = TableModel;
 
 },{}],5:[function(require,module,exports){
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTR, createTH, createTD } = require('./dom-util');
 
 class TableView {
   constructor(model) {
@@ -85,17 +85,40 @@ class TableView {
     this.initDomReferences();
     this.renderTable();
   }
+
   initDomReferences(){
     this.headerRowEl = document.querySelector('THEAD TR');
+    this.sheetBodyEl = document.querySelector('TBODY')
   }
+
   renderTable(){
     this.renderTableHeader();
+    this.renderTableBody();
   }
+
   renderTableHeader(){
     removeChildren(this.headerRowEl);
     getLetterRange('A', this.model.numCols)
       .map(colLabel => createTH(colLabel))
       .forEach(th => this.headerRowEl.appendChild(th));
+  }
+
+  renderTableBody(){
+    const fragment = document.createDocumentFragment();
+    for(let row = 0; row < this.model.numRows; row++){
+      const tr = createTR();
+      for(let col = 0; col < this.model.numCols; col++){
+        const position = {col: col, row: row};
+        const value = this.model.getValue(position);
+        const td = createTD(value);
+        tr.appendChild(td);
+      }
+      fragment.appendChild(tr);
+    }
+
+    removeChildren(this.sheetBodyEl);
+    this.sheetBodyEl.appendChild(fragment);
+
   }
 }
 
