4124342047fb80599f08be0714ab43a87342e102
interdigitize <interdigitize@users.noreply.github.com>
2017-05-01 07:46:00 -0700

    adds render-data-rows unit test

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -8,6 +8,27 @@ describe('table-view', () => {
     const html = fs.readFileSync(fixturePath, 'utf8');
     document.documentElement.innerHTML = html;
   });
+  describe('table body', () => {
+    it('has the right number of rows', () => {
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
+    });
+    it('fills in values from the model', () => {
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({col: 2, row: 1}, '123');
+      view.init();
+
+      const trs = document.querySelectorAll('TBODY TR');
+      expect(trs[1].cells[2].textContent).toBe('123');
+    });
+  });
   describe('table header', () => {
     it('has valid column header labels', () => {
       const numCols = 6;
@@ -23,4 +44,5 @@ describe('table-view', () => {
       expect(labelTexts).toEqual(['A', 'B', 'C', 'D', 'E', 'F']);
     });
   });
+
 });
